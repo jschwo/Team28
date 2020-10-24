0ac4712efd5721bec6b8803ff2d14a23d67aac89
airwheel <aidanwheeler14@gmail.com>
2020-10-24 18:16:02 -0400

    Changes to viewCert
    changed things

--- app/public/api/certifications/getCerts.php
+++ app/public/api/certifications/getCerts.php
@@ -6,23 +6,23 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM Certifcations WHERE certificationID = ?';
+$sql = 'SELECT * FROM Certification';
 $vars = [];
 
 if (isset($_GET['certificationID'])) {
   // This is an example of a parameterized query
-  $sql = 'SELECT * FROM Certifcations WHERE certificationID = ?';
+  $sql = 'SELECT * FROM Certification WHERE certificationID = ?';
   $vars = [ $_GET['certificationID'] ];
 }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$Certs = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($Certs, JSON_PRETTY_PRINT);
 
 // Step 4: Output
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 echo $json;
---
+++ app/public/js/viewCert.app.js
@@ -0,0 +1,60 @@
+certificationApp = new Vue({
+  el: '#generateCert',
+  data: {
+    Cert: [{
+      certificationID: '',
+      certName: '',
+      certAgency: '',
+      expDate: '',
+      renewDate: '',
+      expPeriod: ''
+    }],
+    newCert: {
+      certName: '',
+      certAgency: '',
+      expDate: '',
+      renewDate: '',
+      expPeriod: ''
+    }
+  },
+
+  methods:{
+    fetchCert(){
+      fetch('api/certifications/getCerts.php')
+      .then(response => response.json())
+      .then(json => {
+        this.Cert=json;
+        console.log(this.Cert);
+      });
+  },
+  createCert(){
+      fetch('api/certifications/postCerts.php', {
+      method: 'POST',
+      body: JSON.stringify(this.newCert),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    .then( response => response.json() )
+    .then( json => {
+      console.log("Returned from post:", json);
+      this.Cert=json;
+      this.newCert = this.newCertData();
+    });
+    console.log("Creating (POSTing)...!");
+    console.log(this.newCert);
+  },
+  newCertData() {
+    return {
+      certName: '',
+      certAgency: '',
+      expDate: '',
+      renewDate: '',
+      expPeriod: ''
+      }
+    }
+  },
+  created(){
+    this.fetchCert();
+  }
+});
